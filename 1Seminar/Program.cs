866417e364565272021c458200f29334be9d0c60
Roman Linkov <roman.linkov@vk.com>
2023-03-14 21:08:40 +0300

    Добавлены задачи с семинара

---
+++ 1Seminar/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+
+int number=NumberInput("Введите число: ");
+
+int SumDigits(int n)
+{
+    if (n==0) return 0;
+    else return n%10+SumDigits(n/10);
+}
+
+
+Console.WriteLine($"Сумма цифр: {SumDigits(number)}");
+
+int NumberInput(string msg)
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
---
+++ 2Seminar/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int number=NumberInput("Введите число: ");
+int power=NumberInput("Введите степень числа: ");
+
+int PowerNumber(int n, int p)
+{
+    if (p==0) return 1;
+    else return n*PowerNumber(n, p-1);
+}
+
+
+Console.WriteLine($"Возвдённое в степень: {PowerNumber(number, power)}");
+
+int NumberInput(string msg)
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
